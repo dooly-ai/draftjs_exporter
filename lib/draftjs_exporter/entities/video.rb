34236cc06506632153a21740141b755d7d1826a7
Máté Marjai <mate@theproduct.works>
2021-05-12 11:09:15 +0200

    Change the embed video code to check for youtube watch links and convert those to embed links

--- lib/draftjs_exporter/entities/video.rb
+++ lib/draftjs_exporter/entities/video.rb
@@ -6,11 +6,18 @@ module DraftjsExporter
 
       def initialize(configuration = { className: nil })
         # Default styles for images
-        @configuration = configuration.merge({allowfullscreen: "", frameboder: "0"})
+        @configuration = configuration.merge({allowfullscreen: "1", frameboder: "0", allow: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"})
       end
 
       def call(parent_element, data)
-        args = { src: data.fetch(:data, {}).fetch(:src) }
+        # Check for youtube links, and change watch to embed in order to embed correctly
+        src = data.fetch(:data, {}).fetch(:src)
+        if src && src.include? "https://www.youtube.com/watch"
+          video_id = src.split("=")[1]
+          src = "https://www.youtube.com/embed/#{video_id}"
+        end
+
+        args = { src:  src}
         args[:class] = configuration.fetch(:className) if configuration[:className]
 
         element = parent_element.document.create_element('iframe', args)
